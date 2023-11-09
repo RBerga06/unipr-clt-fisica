2d8987c7b6d3a78f693853a4855f628bd98ef54d
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-09 15:07:55 +0100

    Cout milestones differently

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 /*** Constants ***/
 const N_THREADS: usize = 50;
-const N_MILESTONES: u32 = 10_000;
-const N_THROWS: u32 = 2_000_000;
+const N_MILESTONES: u32 = 1_000_000;
+const N_THROWS: u32 = 20_000;
 const N_ROLLS: u8 = 5;
 const N_BINS: usize = (N_ROLLS + 1) as usize;
+const N_COUNT_TOTAL: u128 = (N_MILESTONES as u128) * (N_THREADS as u128);
 
 /*** Imports ***/
 use std::{sync::mpsc::{self, Sender}, thread, io::{self, Write}};
 use rand::prelude::Distribution;
 
 
-fn work(tx: Sender<(u32, [u128; N_BINS])>) {
+fn work(tx: Sender<[u128; N_BINS]>) {
     /*** Setup ***/
-    println!("work: start");
     let mut rng = rand::thread_rng();
     let die = rand::distributions::Uniform::from(1..7u8);
     /*** Main program ***/
     let mut counter: u8;
     let mut bins: [u128; N_BINS];
-    for milestone in 0..N_MILESTONES {
+    for _milestone in 0..N_MILESTONES {
         bins = [0; N_BINS];
         for _throw in 0..N_THROWS {
             counter = 0;
@@ -30,9 +30,8 @@ fn work(tx: Sender<(u32, [u128; N_BINS])>) {
             }
             bins[counter as usize] += 1;
         }
-        tx.send((milestone, bins)).unwrap();
+        tx.send(bins).unwrap();
     }
-    println!("work: done");
 }
 
 
@@ -41,12 +40,13 @@ fn print_info() {
     println!("#lanci:  {N_THROWS}");
     println!("#volte:  {N_MILESTONES}");
     println!("#thread: {N_THREADS}");
-    let total: u128 = (N_ROLLS as u128)*(N_THROWS as u128)*(N_MILESTONES as u128)*(N_THREADS as u128);
+    let total: u128 = (N_ROLLS as u128)*(N_THROWS as u128)*N_COUNT_TOTAL;
     println!("#totale: {total}");
 }
 
-fn print_bins(bins: [u128; N_BINS]) {
-    print!("->");
+fn print_bins(count: u128, bins: [u128; N_BINS]) {
+    let percent: u128 = (100*count)/N_COUNT_TOTAL;
+    print!("[{count}/{N_COUNT_TOTAL}|{percent}%]");
     for i in 0..N_BINS {
         let bin = bins[i];
         print!("\t{bin}");
@@ -59,24 +59,22 @@ fn print_bins(bins: [u128; N_BINS]) {
 fn main() {
     print_info();
     /*** Spawn threads ***/
-    let (tx, rx) = mpsc::channel::<(u32, [u128;N_BINS])>();
+    let (tx, rx) = mpsc::channel::<[u128;N_BINS]>();
     for _i in 0..N_THREADS {
         let txi = tx.clone();
         thread::spawn(move ||{ work(txi); });
     }
     /*** Combine all outputs ***/
-    let mut done: usize = 0;
+    let mut count: u128 = 0;
     let mut bins: [u128; N_BINS] = [0; N_BINS];
-    for (milestone, bin) in rx {
+    for bin in rx {
         for i in 0..N_BINS {
             bins[i] += bin[i];
         }
-        if milestone == N_MILESTONES - 1 {
-            done += 1;  // another thread has finished!
-        }
-        if done == N_THREADS {
+        count += 1;  // another thread has finished!
+        if count == N_COUNT_TOTAL {
             break;
         }
-        print_bins(bins);
+        print_bins(count, bins);
     }
 }
