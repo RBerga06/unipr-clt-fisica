aa5b95efb124f38dfe3f572da58f5e615ed82934
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-25 15:18:03 +0100

    Render 100 data points

--- lab1/r4-random/src/anim.py
+++ lab1/r4-random/src/anim.py
@@ -22,7 +22,7 @@ from rberga06.phylab.manim.hist import DEFAULT_BAR_COLORS, DiscreteDistributionH
 
 # --- Poisson ---
 
-N_MAX: int | None = 0
+N_MAX: int | None = 100
 FILE = SRC.parent/"data/p1.txt"
 
 
