c85aeffb146f543da93a394e723ad752fa09aa04
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-25 13:10:58 +0100

    Completely rewrite the animations in a sensible way

--- lab1/r4-random/src/anim.py
+++ lab1/r4-random/src/anim.py
@@ -4,10 +4,10 @@
 # pyright: reportMissingTypeStubs=false
 # pyright: reportUnknownMemberType=false
 # pyright: reportUnknownArgumentType=false
+# pyright: reportConstantRedefinition=false
 # ruff: noqa: E402
 """Mathematical ANIMations via MANIM."""
 import sys
-from math import sqrt
 from pathlib import Path
 from typing import Iterator
 from typing_extensions import override
@@ -20,8 +20,6 @@ sys.path.insert(0, str(SRC.parent.parent.parent/".venv/lib/python3.12/site-packa
 
 from rberga06.phylab.poisson import Poisson
 from rberga06.phylab.manim import DEFAULT_BAR_COLORS, hist_discrete as rb06hist
-from utils import Dyn
-from manim_utils import AnimMut, AnimUpd
 
 
 # --- Poisson ---
@@ -36,137 +34,103 @@ def load_data(file: Path) -> Iterator[int]:
         if s and not s.startswith("#")
     )
 
-def y_range(*bins: int) -> tuple[float, float]:
-    ymax = max(1, max(bins) + 1, sum(bins)//2)
-    dy = max(1, ymax//5)
-    return ymax, dy
-
-def mkhist(P: Poisson[int], n: float, d: float) -> BarChart:
-    nbins = len(P.bins)
-    return rb06hist(
-        P,
-        bar_names=[f"{i}" for i in range(nbins)],
-        bar_colors=DEFAULT_BAR_COLORS[:nbins],
-        y_range=[0, n, d],
-    ).shift(DOWN)
-
 def histpt(hist: BarChart, bin: float, y: float) -> Point3D:
     return hist.coords_to_point(bin + .5, y, 0)  # type: ignore
 
-def histpts(hist: BarChart, ys: list[float]) -> Iterator[Point3D]:
-    for i, y in enumerate(ys):
-        yield histpt(hist, i, y)
-
-def mkdot() -> Circle:
-    dot = Circle(DEFAULT_DOT_RADIUS)
-    dot.set_fill(RED_E, opacity=1)
-    dot.set_stroke(BLACK, width=DEFAULT_STROKE_WIDTH*.3)
-    return dot
-
 
 class PoissonScene(Scene):
+    # --- Distribution stats ---
+    P: Poisson[int]
+
+    @property
+    def N(self, /) -> int:
+        return len(self.P.data)
+
+    @property
+    def bins(self, /) -> tuple[int, ...]:
+        return tuple([len(b) for b in self.P.bins])
+
+    @property
+    def nbins(self, /) -> int:
+        return len(self.bins)
+
+    @property
+    def y_range(self, /) -> tuple[int, int]:
+        y = max(1, max(self.bins) + 1, self.N // 2)
+        d = max(1, y // 5)
+        return y, d
+
+    # --- Mobjects ---
+    hist: BarChart
+    hist_avg: DashedLine
+    hist_dots: VGroup
+    texts: VGroup
+
+    def mkhist(self, /) -> BarChart:
+        # Histogram
+        hist = rb06hist(
+            self.P,
+            bar_names=[f"{i}" for i in range(self.nbins)],
+            bar_colors=DEFAULT_BAR_COLORS[:self.nbins],
+            y_range=[0, *self.y_range],
+        ).shift(DOWN)
+        # Average line
+        self.hist_avg = DashedLine(
+            histpt(hist, self.P.average, 0),
+            histpt(hist, self.P.average, self.y_range[0]),
+        ).set_stroke(width=DEFAULT_STROKE_WIDTH*.5).set_color(RED)
+        hist.add(self.hist_avg)
+        # Bar labels
+        hist.bar_labels = hist.get_bar_labels(font_size=28).set_stroke(
+            BLACK, width=DEFAULT_STROKE_WIDTH*.6, background=True,
+        )
+        hist.add(hist.bar_labels)
+        # Theorical dots
+        self.hist_dots = VGroup(*[
+            Circle(DEFAULT_DOT_RADIUS)
+                .set_fill(RED_E, opacity=1)
+                .set_stroke(BLACK, width=DEFAULT_STROKE_WIDTH*.3)
+                .move_to(histpt(hist, i, h))
+            for i, h in enumerate(self.P.expected())
+        ])
+        hist.add(self.hist_dots)
+        # Return
+        return hist
+
+    def mktexts(self, /) -> VGroup:
+        tn = MathTex(f"n = {self.N}").to_edge(UP)
+        ta = MathTex(rf"\mu = {self.P.average:.2f}").next_to(tn, DOWN).set_color(RED)
+        ts = MathTex(rf"\sigma = {self.P.sigma:.2f}").next_to(ta, DOWN).set_color(RED)
+        return VGroup(tn, ta, ts)
+
+    def adding[T: Mobject](self, obj: T, /) -> T:
+        self.add(obj)
+        return obj
+
     @override
     def construct(self) -> None:
-        bins: list[int] = [0]
-        n: int   = 0
-        µ: float = 0
-
-        # --- Column labels ---
-        labels = AnimUpd(Dyn(
-            lambda: hist.get_bar_labels(font_size=28).set_stroke(
-                BLACK, width=DEFAULT_STROKE_WIDTH*.5, background=True,
-            )
-        ), Write, ReplacementTransform, Unwrite)
-
-        # --- Counters ---
-        @AnimUpd.dyn(Write, ReplacementTransform, Unwrite)
-        def text() -> VGroup:
-            tn = MathTex(f"n = {n}")
-            ta = MathTex(rf"\mu = {µ:.2f}").next_to(tn, DOWN).set_color(RED)
-            ts = MathTex(rf"\sigma = {sqrt(µ):.2f}").next_to(ta, DOWN).set_color(RED)
-            return VGroup(tn, ta, ts).to_edge(UP)
-
-        # --- Average line ---
-        avg_line = AnimUpd(
-            Dyn(lambda:
-                DashedLine(histpt(hist, µ, 0), histpt(hist, µ, ymax))
-                    .set_stroke(width=DEFAULT_STROKE_WIDTH*.5)
-                    .set_color(RED)
-            ),
-            DrawBorderThenFill,
-            ReplacementTransform,
-            FadeOut,
-        )
-
-        # --- Theorical dots ---
-        def _dotp(bin: int, /) -> Point3D:
-            return histpt(
-                hist, bin,
-                dist_vals[bin] if bin < len(dist_vals) else 0
-            )
-
-        def mk_dot(bin: int, /) -> AnimMut[Circle]:
-            @AnimMut[Circle].const(
-                lambda dot: DrawBorderThenFill(dot.move_to(_dotp(bin))),  # type: ignore
-                lambda dot: dot.animate.move_to(_dotp(bin)),
-                FadeOut,
-            )
-            def dot() -> Circle:
-                return mkdot()
-            return dot
-
-        # --- Actual animations ---
-        ndots = 0
-        dots: list[AnimMut[Circle]] = []
-        for t, P in enumerate(Poisson.mk_iter_cumulative(load_data(FILE))):
-            bins = [len(b.data) for b in P.bins] or [0]
-            if N_MAX is not None:
-                if t == N_MAX:
-                    break
-            n = t
-            # Make new dots if necessary
-            dots += [mk_dot(i) for i in range(ndots, len(bins)+1 - ndots)]
-            # Distribution fit
-            µ, dist_vals = P.average, [*P.expected()]
-            # Update Mobjects
-            ymax, dy = y_range(*bins)
-            hist_anim = ReplacementTransform(
-                locals().get("hist"), (hist := mkhist(P, ymax, dy))
-            )
-            # Create dots animations
-            dots_anims: list[Animation] = []
-            for i, d in enumerate(dots):
-                if i < ndots:
-                    dots_anims.append(d.morph())
-                else:
-                    dots_anims.append(d.intro())
+        # --- Load data (as an iterator) ---
+        Pit = Poisson.mk_iter_cumulative(load_data(FILE))
+        # --- Intro animations ---
+        self.P = next(Pit)
+        self.hist  = self.adding(self.mkhist())
+        self.texts = self.adding(self.mktexts())
+        everything = VGroup(*self.mobjects)
+        self.play(Write(everything))
+        # --- Transform animations ---
+        for P in Pit:
+            self.P = P
+            if (N_MAX is not None) and (self.N == N_MAX + 1):
+                break
             # Play animations
-            if t == 0:
-                self.play(
-                    DrawBorderThenFill(hist),
-                    text.intro(),
-                    *dots_anims,
-                    avg_line.intro(),
-                    labels.intro(),
-                )
-            else:
-                self.play(
-                    hist_anim,
-                    text.morph(),
-                    *dots_anims,
-                    avg_line.morph(),
-                    labels.morph(),
-                    run_time=self.__run_time(t),
-                )
-            ndots = len(bins)
+            self.play(
+                Transform(self.hist,  self.mkhist()),
+                Transform(self.texts, self.mktexts()),
+                run_time=self.__run_time(self.N),
+            )
+        # --- Outro animations ---
         self.wait(3)
-        self.play(
-            text.outro(),
-            labels.outro(),
-            avg_line.outro(),
-            *[d.outro() for d in dots],
-            Unwrite(hist),
-        )
+        self.play(Unwrite(everything))
         self.wait(.5)
 
     def __run_time(self, t: int) -> float:
