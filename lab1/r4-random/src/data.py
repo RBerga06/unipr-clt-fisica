de4185050671894ca932eba8fd54d6fd6866235b
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-28 16:29:27 +0100

    Relazione

--- lab1/r4-random/src/data.py
+++ lab1/r4-random/src/data.py
@@ -1,11 +1,19 @@
-from math import log as ln
+from math import log as ln, sqrt
 from pathlib import Path
+from typing import Iterator
+from rberga06.phylab.poisson import Poisson
 DIR = Path(__file__).parent
 
 NA = int(6.022e23)
 T12 = 14.5e9 * 365 * 24 * 60 * 60
 Mn = 1.68e-27
 r = 0.007
+
+def load_data(file: Path) -> Iterator[int]:
+    return (
+        int(s) for s in map(str.strip, file.read_text().splitlines())
+        if s and not s.startswith("#")
+    )
 
 def merge(file1: Path, file2: Path, output: Path, sep: str = "\n") -> None:
     output.write_text(file1.read_text() + sep + file2.read_text())
@@ -25,7 +33,18 @@ def geiger(file: Path, R: float, /) -> float:
     print(f"{N=} {Aa=}")
     return N * 232 * Mn
 
-print(T12)
-print(geiger(DIR/"G171.txt", 0.103))
-print(geiger(DIR/"G171.txt", 0.103))
 # merge(DIR/"dadi1.txt", DIR/"dadi2.txt", DIR/"dadi.txt", sep="# --- NEW DATA --- #\n")
+# print(T12)
+# print(geiger(DIR/"G171.txt", 0.103))
+# print(geiger(DIR/"G171.txt", 0.103))
+
+DATA = Path(__file__).parent.parent/"data"
+
+for n in range(6):
+    data = Poisson([*load_data(DATA/f"p{n}.txt")][:3657])
+    print(f"--- File {n} ---")
+    print(f"N  = {len(data)}")
+    print(f"µ  = {data.average}")
+    print(f"σ  = {data.sigma}")
+    print(f"σx = {data.sigma/sqrt(len(data))}")
+print("---------------")
