a1e058bd16623593ce7d18f534d61ffc722ec006
Riccardo Bergamaschi <78449715+RBerga06@users.noreply.github.com>
2024-02-13 22:27:50 +0100

    Update data.py

--- lab1/r5-torsione/data.py
+++ lab1/r5-torsione/data.py
@@ -9,6 +9,7 @@ from typing import NamedTuple
 from rberga06.phylab import Datum, Measure
 from rberga06.phylab.constants import π
 
+-0.0466661825 ± 1.0861373135E-4
 DATA_DIR = Path(__file__).parent / "data"
 
 
