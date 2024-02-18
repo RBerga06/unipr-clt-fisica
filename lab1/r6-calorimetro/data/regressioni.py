5a8a2bc5068436d392e3699f7893a16649cc7811
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2024-02-18 13:19:01 +0100

    Teb.csv

--- lab1/r6-calorimetro/data/regressioni.py
+++ lab1/r6-calorimetro/data/regressioni.py
@@ -1,3 +1,9 @@
+from rberga06.phylab import Datum
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parent))
+from t_eboll import Teb
+
 q1 = Datum(55.1660457307, 0.004603675)
 xi1 = Datum(-0.0027120068, 1.6298245602E-5)
 Teq = xi1 * Datum(69.5, 24.7) + q1  # °C
@@ -5,3 +11,4 @@ m_H2O_eb  = Datum(85.12, 0.03)  # g
 m_H2O_amb = Datum(99.35, 0.02)  # g
 Tamb = Datum(24.7, 0.2)  # °C
 m_eq = m_H2O_eb * (Teb - Teq) / (Teq - Tamb) - m_H2O_amb  # g
+print(m_eq, "g")
--- lab1/r6-calorimetro/data/t_eboll.py
+++ lab1/r6-calorimetro/data/t_eboll.py
@@ -2,10 +2,11 @@ import math
 from rberga06.phylab import Datum, Measure
 
 def ln(d: Measure[float]):
-  return Datum(math.log(d.best), d.delta_rel)
+  return Datum(math.log(d.best), d.delta/d.best)
 
 a = Datum(-71.785524864, 4.2047211397)
 b = Datum(-24.6907231365, 0.6571863538)
 c = Datum(13.1964752899, 2.4802492685)
-x = Datum(992, 1)
-Teb = a - b * ln(x + c)
+x = Datum(992, 1)  # hPa
+Teb = a - b * ln(x + c)  # °C
+Teb = Datum(Teb.best, 0)  # °C
