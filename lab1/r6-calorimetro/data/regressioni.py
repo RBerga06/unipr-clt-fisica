a09d4b415defbb7bc769867c69371fcbea0c61dd
Riccardo Bergamaschi <78449715+RBerga06@users.noreply.github.com>
2024-02-18 12:25:47 +0100

    Create regressioni.py

---
+++ lab1/r6-calorimetro/data/regressioni.py
@@ -0,0 +1,7 @@
+q1 = Datum(55.1660457307, 0.004603675)
+xi1 = Datum(-0.0027120068, 1.6298245602E-5)
+Teq = xi1 * Datum(69.5, 24.7) + q1  # °C
+m_H2O_eb  = Datum(85.12, 0.03)  # g
+m_H2O_amb = Datum(99.35, 0.02)  # g
+Tamb = Datum(24.7, 0.2)  # °C
+m_eq = m_H2O_eb * (Teb - Teq) / (Teq - Tamb) - m_H2O_amb  # g
