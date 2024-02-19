5b02f98a90963bda7867e326918206326fbd42c1
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2024-02-19 14:09:37 +0100

    Calorimetro

--- lab1/r6-calorimetro/src/Tp.py
+++ lab1/r6-calorimetro/src/Tp.py
@@ -8,25 +8,29 @@ c_aq = 4186  # J/(kg · K)
 def c(Teq: Measure[float], Taq: Measure[float], m_met: Measure[float], m_aq: Measure[float]):
     ms = (m_aq + m_eq) / m_met
     Ts = (Teq - Taq) / (Teb - Teq)
-    return ms * c_aq * Ts
+    print("* Teq - Taq:", (Teq - Taq).delta_rel)
+    print("* Teb - Teq:", (Teb - Teq).delta_rel)
+    c_met = ms * c_aq * Ts
+    print("-> c_met:", c_met)
+    return c_met
 
 cs = [
-    c(
-        Teq=Datum(),
-        Taq=Datum(),
-        m_met=Datum(),
-        m_aq=Datum()
+    c(  # grigio chiaro => acciaio inox
+        Teq=Datum(25.2, 0.1),   # ± 0.2
+        Taq=Datum(24.7, 0.1),
+        m_met=Datum(12.43, 0.01),  # ± 0.02
+        m_aq=Datum(197.54, 0.02),
     ),
-    c(
-        Teq=Datum(),
-        Taq=Datum(),
-        m_met=Datum(),
-        m_aq=Datum()
+    c(  # color ottone => ottone (Cu + Zn)
+        Teq=Datum(25.9, 0.1),
+        Taq=Datum(25.1, 0.1),
+        m_met=Datum(28.73, 0.01),
+        m_aq=Datum(194.81, 0.02),
     ),
-    c(
-        Teq=Datum(),
-        Taq=Datum(),
-        m_met=Datum(),
-        m_aq=Datum()
+    c(  # grigio scuro => piombo (Pb)
+        Teq=Datum(26.0, 0.1),
+        Taq=Datum(25.6, 0.1),
+        m_met=Datum(44.86, 0.01),
+        m_aq=Datum(194.00, 0.02),
     ),
 ]
--- lab1/r6-calorimetro/src/regressioni.py
+++ lab1/r6-calorimetro/src/regressioni.py
@@ -6,9 +6,9 @@ from Teb import Teb
 
 q1 = Datum(55.1660457307, 0.004603675)
 xi1 = Datum(-0.0027120068, 1.6298245602E-5)
-Teq = xi1 * Datum(69.5, 24.7) + q1  # °C
+Teq = xi1 * Datum(69.5, 0.1) + q1  # °C
 m_H2O_eb  = Datum(85.12, 0.03)  # g
 m_H2O_amb = Datum(99.35, 0.02)  # g
-Tamb = Datum(24.7, 0.2)  # °C
+Tamb = Datum(24.7, 0.1)  # °C
 m_eq = m_H2O_eb * (Teb - Teq) / (Teq - Tamb) - m_H2O_amb  # g
 print(m_eq, "g")
