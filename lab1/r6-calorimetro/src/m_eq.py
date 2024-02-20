4045d42dee6fafaad513762375590c8e88fc393a
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2024-02-20 18:52:27 +0100

    Pesetti

--- lab1/r6-calorimetro/src/m_eq.py
+++ lab1/r6-calorimetro/src/m_eq.py
@@ -7,7 +7,7 @@ from Teb import Teb
 intercetta = Datum(55.1825093636, 0.00147224629)
 coeff = Datum(-0.0027656105, 5.0117286396E-5)
 Teq = coeff * Datum(69.5, 0.5) + intercetta  # °C
-# Teq = Datum(Teq.best, 0.1)  # °C
+Teq = Datum(Teq.best, 0.1)  # °C
 print("(m_eq) Teq  =", Teq, "°C")
 m_H2O_eb  = Datum(85.12, 0.03)  # g
 m_H2O_amb = Datum(99.35, 0.02)  # g
